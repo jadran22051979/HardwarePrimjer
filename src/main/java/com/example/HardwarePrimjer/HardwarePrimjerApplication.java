b6cfc3bf8f8a63a124b874a871a503691c53cab1
jadran22051979 <jadran.bonacic@gmail.com>
2026-02-20 22:59:18 +0100

    Commit 2

--- src/main/java/com/example/HardwarePrimjer/HardwarePrimjerApplication.java
+++ src/main/java/com/example/HardwarePrimjer/HardwarePrimjerApplication.java
@@ -2,7 +2,7 @@ package com.example.HardwarePrimjer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Pokretanje aplikacije
 @SpringBootApplication
 public class HardwarePrimjerApplication {
 
